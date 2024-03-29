4ee694a37419d4f0f1d6facf0704d8ed8e8c961b
Piotr Brudny <pbrudny@gmail.com>
2024-03-29 15:55:42 +0100

    Add dynamic chart

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from taipy import Gui
+from math import cos, exp
 
-value = 10
 
 page = """
 #This is *Taipy* GUI
 
-A value: <|{value}|>.
+A value: <|{decay}|>.
 
 A slider: <br />
-<|{value}|slider|>
+<|{decay}|slider|>
+
+My chart:
+<|{data}|chart|>
 """
+
+def compute_data(decay):
+    return [cos(i/16) * exp(-i*decay/6000) for i in range(720)]
+
+# listens for changes in a page and updates the state
+def on_change(state, var_name, var_value):
+    if var_name == 'decay':
+        computed = compute_data(var_value)
+        state.data = computed
+
+
+decay = 10
+data = compute_data(decay) 
 
 Gui(page=page).run()
